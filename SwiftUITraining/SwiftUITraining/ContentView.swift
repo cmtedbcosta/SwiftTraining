306e1bc71f2e13de4f07d7d1cf1f8c42a65fdfdf
Hans Dubois <hansdub@gmail.com>
2020-08-19 13:42:21 +0200

    WIP - Step 1

--- SwiftUITraining/SwiftUITraining/ContentView.swift
+++ SwiftUITraining/SwiftUITraining/ContentView.swift
@@ -10,7 +10,13 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        VStack(alignment: .leading) {
+            Text("Mercury")
+                .font(Font.custom("Helvetica", size: 20))
+            Text("The swiftest planet")
+                .font(Font.custom("Helvetica", size: 13))
+                .foregroundColor(.gray)
+        }
     }
 }
 
